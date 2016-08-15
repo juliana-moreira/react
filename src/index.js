8945b844f71b88cd670d959188fb00df1ee978ac
juliana-moreira <juliana.moreira@objectedge.com>
2016-08-15 20:33:28 -0300

    #1 : Creating the react tutorial repository

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Panel extends React.Component {
+
+  render() {
+    return (
+      <div>Test</div>
+    );
+  }
+}
+
+ReactDOM.render(<Panel />, document.getElementById('root'));
