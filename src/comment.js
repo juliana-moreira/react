e19bc179834c6d360ab0ce1551d2b0aaa99c026f
juliana-moreira <juliana.moreira@objectedge.com>
2016-08-17 21:54:55 -0300

    Receiving data from JSON - Finished

---
+++ src/comment.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Remarkable from 'remarkable';
+
+export class Comment extends React.Component {
+  rawMarkup() {
+    var md = new Remarkable();
+    var rawMarkup = md.render(this.props.children.toString());
+    return { __html: rawMarkup };
+  }
+
+  render() {
+    var md = new Remarkable();
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        <span dangerouslySetInnerHTML={this.rawMarkup()} />
+      </div>
+    );
+  }
+}
---
+++ src/commentForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export class CommentForm extends React.Component {
+  render() {
+    return (
+      <div className="commentForm">
+        Hello, world! I am a CommentForm.
+      </div>
+    );
+  }
+}
