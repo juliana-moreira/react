a773c0a56a16854b8842531fed031758a6fd7f5a
juliana-moreira <juliana.moreira@objectedge.com>
2016-08-17 21:40:19 -0300

    Receiving data from JSON - First part

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {CommentList} from './commentList';
+import {CommentForm} from './commentForm';
 
-class Panel extends React.Component {
+var data = [
+  {id: 1, author: "Pete Hunt", text: "This is one comment"},
+  {id: 2, author: "Jordan Walke", text: "This is *another* comment"}
+];
 
-  render() {
-    return (
-      <div>Test</div>
-    );
-  }
+class CommentBox extends React.Component {
+	render() {
+		return (
+			<div className="commentBox">
+				<h1>Comments</h1>
+        <CommentList data={this.props.data} />
+        <CommentForm />
+			</div>
+		);
+	}
 }
 
-ReactDOM.render(<Panel />, document.getElementById('root'));
+ReactDOM.render (
+	<CommentBox data={data} />,
+	document.getElementById('content')
+);
